471f33fd621b6728e704133631c7ea3f0245691c
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-05-08 10:31:31 -0700

    More indexing and slicing

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "python is an interpreted, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[26:53] + str[-23:-17] + str[0:6]
+print(str)
