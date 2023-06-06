8b2daccb75442997f31feeeeb26d0ca5150ec713
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-06-06 03:05:28 +0000

    More classe

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,7 +4,10 @@
 class Rectangle:
     """The class reps a rectangle"""
     number_of_instances = 0
-    print_symbol = '#'                                                                                                    def __init__(self, width=0, height=0):                         """ The method initializes the class with 2
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """ The method initializes the class with 2
                 parameters:
                         width - reps the width
                         height - reps the  height
@@ -13,7 +16,7 @@ class Rectangle:
                         ValueError if value is < 0
         """
         self.width = width
-        self.height = heigh
+        self.height = height
         type(self).number_of_instances += 1
 
     @property
@@ -46,38 +49,49 @@ class Rectangle:
         else:
             self.__height = value
 
-    def area(self):                                                """The method returns the area"""                          return self.__height * self.__width                                                                               def perimeter(self):                                           """Returns the perimetet of a rectangle"""                 if self.__width == 0 or self.__height == 0:
+    def area(self):
+        """The method returns the area"""
+        return self.__height * self.__width
+    def perimeter(self):
+        """Returns the perimetet of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__height + self.__width)
 
-    def __str__(self):                                             """ Returns string representation """                      if self.__width == 0 or self.__height == 0:
-            return ""                                              symbol = str(self.print_symbol)
-        return "\n".join([symbol * self.width] * self.heigh
-t)                                                                                                                        def __repr__(self):
+    def __str__(self):
+        """ Returns string representation """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        symbol = str(self.print_symbol)
+        return "\n".join([symbol * self.width] * self.height)
+    def __repr__(self):
         """ Returns str rep of a rectangle that can                        create a new instance
         """
-        return "Rectangke({}, {})".format(self.width, self.
-height)
+        return "Rectangke({}, {})".format(self.width, self.height)
 
     def __del__(self):
         """ prints a message when an instance is
                 deleted
         """
-        type(self).number_of_instances -= 1                        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ Returns the biggest  rectangle based
                 on their areas
-        """                                                        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of
-Rectangle")
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         area_1 = rect_1.area()
         area_2 = rect_2.area()
-        if area_1 >= area_2:                                           return rect_1                                          else:                                                          return rect_2
+        if area_1 >= area_2:
+            return rect_1
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
