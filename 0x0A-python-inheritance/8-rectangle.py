bfa2a538b842f40dbf4e2f654d79ded64c56b4d5
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-06-13 02:43:10 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 
+    This Module runs a subclass of the BaseGeometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+#Import the class BaseGeometry from the Module
+
+class Rectangle(BaseGeometry):
+
+    """ This derived class inherits from BaseGeometry """
+    #Constractor
+    def __init__(self, width, height):
+        """ - This Method initializes the class with 
+	    the arguments width and height
+	    - They both have to be private with no getter
+	    or setter.
+	    - both values must be validated  by
+	    integer_validator
+        """
+        if self.integer_validator("Width", width):
+            self.__width = width
+        if self.integer_validator("Height", height):
+            self.__height = height
