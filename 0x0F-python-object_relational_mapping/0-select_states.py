343ef7a8ba9012b0b4a5287821464a2f2799b302
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-08-24 06:11:43 +0300

    changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-"""
-The script that lists all states from 
-the database hbtn_0e_0_usa:
-"""
-#- The script should take 3 arguments 
-#- You must use the module MySQLdb
-#- Your script should connect to a MySQL 
-#server running on localhost at port 3306
-#- Results must be sorted in ascending 
-#order by states.id
+"""The script that lists all states"""
 
 import MySQLdb
 from sys import argv
@@ -23,7 +14,7 @@ if __name__ == '__main__':
 
     """ Create a Cursor """
 
-    myCursor = db.cursor()
+    myCursor = my_db.cursor()
 
     """ Execute the querry """
     myCursor.execute("SELECT * FROM states")
@@ -31,3 +22,6 @@ if __name__ == '__main__':
     rows = myCursor.fetchall()
     for row in rows:
         print(row)
+
+    myCursor.close()
+    my_db.close()
