3e0a2afbe1d9f673358685a0894e6e5604f0a2c8
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-08-31 09:53:12 -0700

    Display all methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i Allow | awk '{for (i=2; i<=NF; i++) print $1}'
+curl -sI "$1" | grep -i Allow | awk '{for (i=2; i<=NF; i++) print $2}'
