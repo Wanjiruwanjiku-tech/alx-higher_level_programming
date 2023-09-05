87178b9eb8b706511496b1dda1677a93ea27ea02
Natalie Wanjiru Wanjiku <119385730+Wanjiruwanjiku-tech@users.noreply.github.com>
2023-09-05 19:12:17 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// An Empty class that defines a Rectangle
-
-class Rectangle {
-  // Empty Class
-}
-module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// A function that converts a number from base 10 to
-// another base passed as argument
-
-exports.converter = function (base) {
-  return function (number) {
-    return number.toString(base);
-  };
-};
