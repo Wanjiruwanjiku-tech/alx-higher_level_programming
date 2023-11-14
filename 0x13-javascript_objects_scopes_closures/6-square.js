b0e96fcbe8aa1a6b2843427156055af1b691a87e
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-11-14 10:42:29 -0800

    Javascript OPP

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,16 @@
 const Square = require('./5-square');
 
 class SquareWithCharPrint extends Square {
-	constructor (size) {
-		super(size);
-	}
+  constructor (size) {
+    super(size);
+  }
 
-	charPrint(c) {
-		// Check if c is undefined
-		const myChar = c || 'X';
-		for (let i = 0; i < this.size; i++) {
-			console.log(myChar.repeat(this.size));
-		}
-	}
+  charPrint (c) {
+    // Check if c is undefined
+    const myChar = c || 'X';
+    for (let i = 0; i < this.size; i++) {
+      console.log(myChar.repeat(this.size));
+    }
+  }
 }
 module.exports = SquareWithCharPrint;
