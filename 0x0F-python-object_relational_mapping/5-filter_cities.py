3108cd5ccc258b14f4338074452c516b4325a843
Natalie Wanjiru Wanjiku <119385730+Wanjiruwanjiku-tech@users.noreply.github.com>
2023-11-18 21:27:49 +0300

    update

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+The Module runs a script that takes in the name of a state as an arg and
+lists all cities of that state
+the script connects to a MySQL server running on localhost on port 3306
+"""
+import MySQLdb
+import sys
+
+def list_cities_by_state(username, password, database, stateName):
+    """
+    The function takes 4 args:
+    username = mysql username,
+    password = mysql password.
+    database = myql database name,
+    stateName = name to be searched 
+    should be SQL Injection free
+    """
+    # Create a connection
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    cursor = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities \
+        JOIN states ON cities.state_id \
+            WHERE states.name = %s \
+                ORDER BY cities.id ASC"
+    cursor.execute(query, ())
+    # Fetch 
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    username, password, database, stateName = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    list_cities_by_state(username, password, database, stateName)
