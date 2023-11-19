1d24ae7ccc24feab6cc0ec4452ba37204626d661
Natalie Wanjiru Wanjiku <119385730+Wanjiruwanjiku-tech@users.noreply.github.com>
2023-11-19 11:12:20 +0300

    Filtering items

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+The Module runs a script that listas all state objects that contain the letter 'a'
+The module must use sqlalchemy
+State and Base classes must be imported from model_state
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def filter_states(username, password, database):
+    """
+    The function takes three args namely:
+    username = mysql username
+    password = mysql password
+    database = the specified database
+    """
+    # Create a connection to the mysql server
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(username, password, database))
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query State and find all with 'a'
+    states =  session.query(State).order_by(State.id).filter(State.name.ilike(%a%)).all()
+    for state in states:
+        print("{}: {}".format(states.id, states.name))
+    
+    # Close Connection
+    session.close()
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    filter_states(username, password, database)
