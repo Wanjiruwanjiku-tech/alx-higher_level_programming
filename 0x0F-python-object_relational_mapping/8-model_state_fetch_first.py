213890d4aaa075bbb8c094c9c6bae4b5d21a02ae
Natalie Wanjiru Wanjiku <119385730+Wanjiruwanjiku-tech@users.noreply.github.com>
2023-11-19 10:13:02 +0300

    Display the first state in a database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module runs a scriptt that prints the first State from the database
+The script must use SQLAlchemy
+State and Base classes must be imported from model_state module
+The state displayed must be first in state.id
+You are not allowed to fetch all before displaying the result
+If the table states is empty, print nothing followed by a new line
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+fromsqlalchemy.orm import sessionmaker
+
+if __name__ =="__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Create a connection to MySQL server
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(username, password, database))
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Querry state objects and display the first result
+    firstState= session.query(State).order_by(State.id).first()
+    if firstState:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("\n")
+    
+    # Close the session
+    session.close()
