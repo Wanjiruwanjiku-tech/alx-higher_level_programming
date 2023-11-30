516d57c11538af697e8b6f1bbc03e766c202cbf5
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-11-30 09:27:26 -0800

    Sending a POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #The script sends a post request and adds two variables wit their respective values
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
