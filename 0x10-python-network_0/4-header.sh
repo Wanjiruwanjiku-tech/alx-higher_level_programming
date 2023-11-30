ef39f44aad4a0cfd58edfc9e6e0dfdda95c8fac4
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-11-30 09:17:28 -0800

    Adding a new Header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #The script takes in a url, sends a Get request and displays the body of the response And sends info
-curl -sL "$1" -H "X-School-User-Id:98"
+curl -sH "X-School-User-Id:98" "$1"
