bfd976c1442b152b798f9b3e7724208672878935
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 10:03:10 -0800

    Header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,11 @@ it then displays the value of X-Request-Id var found in the header response
 import urllib.request
 import sys
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    header_name = 'X-Request-Id'
-    if header_name in response.headers:
-        print(response.headers[header_name])
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        header_name = 'X-Request-Id'
+        print(dict(response.headers).get(header_name))
