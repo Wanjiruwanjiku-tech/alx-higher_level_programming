10b061980ab863806267f12fd00afad790353f0a
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 21:58:34 +0300

    Handling requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,8 @@ import requests
 if __name__ == "__main__":
     
     response = requests.get('https://alx-intranet.hbtn.io/status')
+    decoded = response.content.decode('utf-8')
+
     print("Body response:")
-    print("\t- type {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type {}".format(type(decoded)))
+    print("\t- content: {}".format(decoded))
