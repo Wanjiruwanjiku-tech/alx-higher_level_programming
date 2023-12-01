56738155f7e2dac7fd32ee117692b12ab4387cb1
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 09:45:41 -0800

    Fetch a URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,15 @@ This module runs a script that fetches a url using the urllib package
 """
 # Import necessary modules
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    # Read the response body
-    content = response.read().decode("utf-8")
-    # Display the response body as stated
-    print("Body response")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content)
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        # Read the response body
+        content = response.read().decode("utf-8")
+
+        # Display the response body as stated
+        print("Body response")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content)
