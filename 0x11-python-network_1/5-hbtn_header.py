44346002ed031cea8e2df5ff06b2038b7b9f16ed
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 22:17:32 +0300

    Request headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+"""
+Uses the requests and sys packages to send a request and display the values
+of a variable X-Request-Id
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+
+    url = argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
