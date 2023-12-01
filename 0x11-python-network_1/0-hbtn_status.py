ebb05f91dd40bd30a66403529d9a5793a8e7fa00
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 08:50:50 -0800

    Fetch a url and display its body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,9 @@ This module runs a script that fetches a url using the urllib package
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     # Read the response body
-    content = response.read()
+    content = response.read().decode("utf-8")
     # Display the response body as stated
     print("Body response")
     print("\t- type:", type(content))
     print("\t- content:", content)
+    print("\t- utf8 content:", content)
