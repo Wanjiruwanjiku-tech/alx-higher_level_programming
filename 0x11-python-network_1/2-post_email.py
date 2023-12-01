b8b922bd271654c7adb3f085bf0d1c8d4aa950dc
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-12-01 09:41:06 -0800

    POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,18 +7,23 @@ Decoded inutf-8
 import urllib.request
 import urllib.parse
 import sys
+if __name__ == "__main__":
 
-# Get the url and email
-url = sys.argv[1]
-email = sys.argv[2]
-# Set the values to encode
-values = {
+    # Get the url and email
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Set the values to encode
+    values = {
     'email' : email
-}
-# Encode the values
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-# Fetch the url with a POST request
-with urllib.request.urlopen(url, data) as response:
-    content = response.read().decode('utf-8')
-    print('Your email is: {}'.format(content))
+    }
+
+    # Encode the values
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+
+    # Fetch the url with a POST request
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(url, data) as response:
+        content = response.read().decode('utf-8')
+        print('Your email is: {}'.format(content))
