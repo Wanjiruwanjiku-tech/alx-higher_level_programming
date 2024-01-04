dc69241d09d3eba5ed9175c58ffeee95e7d12b7e
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-04 05:39:02 -0800

    Read file contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 // Import the fs module from Node.js
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', function(err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
