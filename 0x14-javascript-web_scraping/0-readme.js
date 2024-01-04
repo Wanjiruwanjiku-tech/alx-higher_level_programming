80d287a59385943e9e92021fc222cf7d6112f36d
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-04 05:36:08 -0800

    Read file contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,7 @@
 // The script reads and prints the content of a file.
 
 // Import the fs module from Node.js
-import fs, { lstat } from 'fs';
-
+const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', function(err, data) {
     if (err) {
