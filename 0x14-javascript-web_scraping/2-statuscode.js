7222a727f3db14d77570450d68a60b0f173f482c
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-04 17:04:37 +0300

    Web scrapping in JavaScript

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// The script displays the status code of a GET Request
+// Import the required modules
+
+const request = require('request');
+
+// Store the url in question
+const url = process.argv[2];
+
+request(url, function(err, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('code: ' + response.statusCode);
+    }
+});
